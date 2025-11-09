98795230c147f5bd07306d5cf0d3894e0445b5da
leongmal <leonik1968@gmail.com>
2025-11-09 12:47:21 +0300

    удален не используемый код

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selenium import webdriver
 from pages.page_main import MainPage
-# from locators.locator_main import MainPageLocators
 from data import Urls
+
 
 @pytest.fixture(scope="function")
 def driver():
@@ -12,10 +12,8 @@ def driver():
     browser.quit()
 
 
-
 @pytest.fixture(scope="function")
 def main_page(driver):
     page = MainPage(driver)
     page.wait_for_load_page()
-    # page.wait_for_load_page(MainPageLocators.TITLE_IMPORTANT_QUESTIONS)
     return page
